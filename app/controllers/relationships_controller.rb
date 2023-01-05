c4c71eb5a9af6ac10adc4c0e7d12401ea24972f1
shalini <shalinip@shriffle.com>
2023-01-05 19:05:36 +0530

    added follow unfollow button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,22 +12,20 @@ class RelationshipsController < ApplicationController
   end
 
   def manage
-    byebug
-    # @followee=params[:user]
     @user=User.find_by_id(params[:user])
-    allid=@current_user.followees.ids
-    if allid.include?(@user.id)
+    all_id=@current_user.followees.ids
+    if all_id.include?(@user.id)
       redirect_to search_path(token:@token)
     else
       Relationship.create(follower_id:@current_user.id,followee_id:@user.id)
-      redirect_to following_path(token: @token)
+      redirect_to search_path(token: @token)
     end
   end
 
   def deletem
     @followee=params[:user]
     Relationship.delete_by(follower_id:@current_user.id,followee_id:@followee)
-    redirect_to following_path(token: @token)
+    redirect_to search_path(token: @token)
   end
 
 end
